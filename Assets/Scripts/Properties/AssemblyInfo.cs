670b8d410d875823b5ccedcbf76b3148f0a880dd
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-08-18 00:24:44 +0200

    Fixed compiler error from accidentally including compiler generated code

--- Assets/Scripts/Properties/AssemblyInfo.cs
+++ Assets/Scripts/Properties/AssemblyInfo.cs
@@ -1,3 +1,1 @@
-[assembly: global::Unity.Burst.BurstCompiler.StaticTypeReinit(typeof(BurstFunctions.Average_00000075_0024BurstDirectCall))]
-[assembly: global::Unity.Burst.BurstCompiler.StaticTypeReinit(typeof(BurstFunctions.Average_00000076_0024BurstDirectCall))]
 [assembly: global::System.Reflection.AssemblyVersion("0.0.0.0")]
